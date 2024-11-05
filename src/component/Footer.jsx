425f6afedd4e5b656acfe2171656cbdd86bf93f0
PhithLoveSandwich <664259020@webmail.npru.ac.th>
2024-11-05 16:27:28 +0700

    ครั้งที่4

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,47 +1,51 @@
 import React from "react";
 const Footer = () => (
   <footer>
-    <div className="top-footer">
-      <p>Thanawat Srisaeng .</p>
-    </div>
-    <div className="middle-footer">
-      <ul className="footer-menu">
-        <li className="footer_menu_list">
-          <a href="#home">Home</a>
-        </li>
-        <li className="footer_menu_list">
-          <a href="#about">About</a>
-        </li>
-        <li className="footer_menu_list">
-          <a href="#project">Project</a>
-        </li>
-        <li className="footer_menu_list">
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </div>
-    <div className="footer-social-icons">
-      <div className="icon">
-        <a
-          href="https://www.facebook.com/thanawad.srisaeng.35/?locale=th_TH"
-          className="icon social-icon"
-        >
-          <i className="uil uil-facebook"></i>
-        </a>
-      </div>
-      <div className="icon">
-        <a href="#" className="icon social-icon">
-          <i className="uil uil-twitter"></i>
-        </a>
-      </div>
-      <div className="icon">
-        <a href="#" className="icon social-icon">
-          <i className="uil uil-linkedin"></i>
-        </a>
+    <div className="newsletter-section">
+      <h3>Subscribe to our newsletter</h3>
+      <div className="newsletter-form">
+        <input type="email" placeholder="Your Email Address" />
+        <button>Subscribe</button>
       </div>
     </div>
-    <div className="bottom-footer">
-      <p>Copyright &copy;Thanawat - All right reserved</p>
+
+    <div className="footer-content">
+      <div className="footer-column">
+        <p>Thanawat Shampoo Shop</p>
+      </div>
+
+      <div className="footer-column">
+        <h4>Quick Link</h4>
+        <ul>
+          <li><a href="#plants">Soap</a></li>
+          <li><a href="#flowers">Shampoo</a></li>
+        </ul>
+      </div>
+
+      <div className="footer-column">
+        <h4>Popular Services</h4>
+        <ul>
+          <li>ติดต่อประชาสัมพันธ์</li>
+          <li>สั่งซื้อ</li>
+        </ul>
+      </div>
+
+      <div className="footer-column">
+        <h4>Contact Us</h4>
+        <p>+88 855564258</p>
+        <p>664259020@gmail.com</p>
+        <p>1142 Thailand Ave, CBS City</p>
+      </div>
+    </div>
+
+    <div className="footer-social">
+      <a href="https://facebook.com"><i className="uil uil-facebook"></i></a>
+      <a href="https://twitter.com"><i className="uil uil-twitter"></i></a>
+      <a href="https://linkedin.com"><i className="uil uil-linkedin"></i></a>
+    </div>
+
+    <div className="footer-bottom">
+      <p>Copyright &copy; Thanawat - All rights reserved</p>
     </div>
   </footer>
 );
